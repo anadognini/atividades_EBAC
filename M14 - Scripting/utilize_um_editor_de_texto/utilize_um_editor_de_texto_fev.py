eac8bbecd4e7262d8091bc960a9275adf6915512
Ana <92342881+anadognini@users.noreply.github.com>
2025-11-24 16:24:20 -0300

    Update utilize_um_editor_de_texto_fev.py

--- M14 - Scripting/utilize_um_editor_de_texto/utilize_um_editor_de_texto_fev.py
+++ M14 - Scripting/utilize_um_editor_de_texto/utilize_um_editor_de_texto_fev.py
@@ -8,7 +8,7 @@ import os
 
 # Função para criar os gráficos
 
-def analise_sinasc_fevereiro_2019(df, value, index, aggfunc, ylabel, xlabel, opcao='nada'):
+def analise_mensal(df, value, index, aggfunc, ylabel, xlabel, opcao='nada'):
    if opcao == 'nada':
         pd.pivot_table(df, values=value, index=index, aggfunc=aggfunc).plot(figsize=[15, 5])
    elif opcao == 'sort':
@@ -23,40 +23,41 @@ def analise_sinasc_fevereiro_2019(df, value, index, aggfunc, ylabel, xlabel, opc
 
 # Lendo o dataframe
 
-df_fevereiro = pd.read_csv('C:/Users/anado/OneDrive/Documentos/EBAC (2)/M14 - Scripting/SINASC_RO_2019_FEV.csv')
+sinasc = pd.read_csv('C:/Users/anado/OneDrive/Documentos/EBAC (2)/Dados/SINASC_RO_2019_FEV.csv')
 
-# Criando um diretório apenas para o mês de fevereiro
+# Criando um diretório apenas para o mês em questão
 
-max_data = df_fevereiro.DTNASC.max()[:7]
+max_data = sinasc.DTNASC.max()[:7]
 max_data
+
 os.makedirs('C:/Users/anado/OneDrive/Documentos/EBAC (2)/M14 - Scripting/output/figs/' + max_data, exist_ok=True)
 
 # Criando os gráficos e salvando-os na pasta criada
 
-analise_sinasc_fevereiro_2019(df_fevereiro, 'IDADEMAE', 'DTNASC', 'mean',
+analise_mensal(sinasc, 'IDADEMAE', 'DTNASC', 'mean',
                             'Média de Idade da Mãe', 'Data')
-plt.savefig('C:/Users/anado/OneDrive/Documentos/EBAC (2)/M14 - Scripting/output/figs/2019-02/' + 'media_idade_mae.png')
+plt.savefig('C:/Users/anado/OneDrive/Documentos/EBAC (2)/M14 - Scripting/output/figs/' + max_data + '/media_idade_mae.png')
 
-analise_sinasc_fevereiro_2019(df_fevereiro, 'IDADEMAE', 'DTNASC', 'count',
+analise_mensal(sinasc, 'IDADEMAE', 'DTNASC', 'count',
                             'Quantidade de Nascimentos', 'Data')
-plt.savefig('C:/Users/anado/OneDrive/Documentos/EBAC (2)/M14 - Scripting/output/figs/2019-02/' + 'qtd_nascimentos.png')
+plt.savefig('C:/Users/anado/OneDrive/Documentos/EBAC (2)/M14 - Scripting/output/figs/' + max_data + '/qtd_nascimentos.png')
 
-analise_sinasc_fevereiro_2019(df_fevereiro, 'PESO', 'ESCMAE', 'median',
+analise_mensal(sinasc, 'PESO', 'ESCMAE', 'median',
                             'Peso Mediano', 'Escolaridade da Mãe')
-plt.savefig('C:/Users/anado/OneDrive/Documentos/EBAC (2)/M14 - Scripting/output/figs/2019-02/' + 'peso_escolaridade_mae.png')
+plt.savefig('C:/Users/anado/OneDrive/Documentos/EBAC (2)/M14 - Scripting/output/figs/' + max_data + '/peso_escolaridade_mae.png')
 
-analise_sinasc_fevereiro_2019(df_fevereiro, 'APGAR1', 'GESTACAO', 'mean',
+analise_mensal(sinasc, 'APGAR1', 'GESTACAO', 'mean',
                             'APGAR1', 'Tempo de Gestação', 'sort')
-plt.savefig('C:/Users/anado/OneDrive/Documentos/EBAC (2)/M14 - Scripting/output/figs/2019-02/' + 'apgar1_tempo_gestacao.png')
+plt.savefig('C:/Users/anado/OneDrive/Documentos/EBAC (2)/M14 - Scripting/output/figs/' + max_data + '/apgar1_tempo_gestacao.png')
 
-analise_sinasc_fevereiro_2019(df_fevereiro, 'APGAR5', 'GESTACAO', 'mean',
+analise_mensal(sinasc, 'APGAR5', 'GESTACAO', 'mean',
                             'APGAR5', 'Tempo de Gestação', 'sort')
-plt.savefig('C:/Users/anado/OneDrive/Documentos/EBAC (2)/M14 - Scripting/output/figs/2019-02/' + 'apgar5_tempo_gestacao.png')
+plt.savefig('C:/Users/anado/OneDrive/Documentos/EBAC (2)/M14 - Scripting/output/figs/' + max_data + '/apgar5_tempo_gestacao.png')
 
-analise_sinasc_fevereiro_2019(df_fevereiro, 'IDADEMAE', ['DTNASC', 'SEXO'], 'mean',
+analise_mensal(sinasc, 'IDADEMAE', ['DTNASC', 'SEXO'], 'mean',
                             'Média Idade Mãe', 'Data', 'unstack')
-plt.savefig('C:/Users/anado/OneDrive/Documentos/EBAC (2)/M14 - Scripting/output/figs/2019-02/' + 'qtd_nascimentos_sexo.png')
+plt.savefig('C:/Users/anado/OneDrive/Documentos/EBAC (2)/M14 - Scripting/output/figs/' + max_data + '/qtd_nascimentos_sexo.png')
 
-analise_sinasc_fevereiro_2019(df_fevereiro, 'PESO', ['DTNASC', 'SEXO'], 'count',
+analise_mensal(sinasc, 'PESO', ['DTNASC', 'SEXO'], 'count',
                             'Média Idade Mãe', 'Data', 'unstack')
-plt.savefig('C:/Users/anado/OneDrive/Documentos/EBAC (2)/M14 - Scripting/output/figs/2019-02/' + 'peso_sexo.png')
+plt.savefig('C:/Users/anado/OneDrive/Documentos/EBAC (2)/M14 - Scripting/output/figs/' + max_data + '/peso_sexo.png')
